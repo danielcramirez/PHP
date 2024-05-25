07188c9b31b60a62adb1e9a4ace3b6a2a86ced18
Daniel Camilo Ramirez Martinez <160069324+danielcramirez@users.noreply.github.com>
2024-05-25 07:52:54 -0500

    Create hoja de vida.php

---
+++ hoja de vida.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Hoja de Vida</title>
+	<style>
+		body {
+			background-color: lightblue;
+		}
+		h1 {
+			color: white;
+			text-align: center;
+		}
+		p {
+			font-family: verdana;
+			font-size: 20px;
+		}
+		.imgbox {
+			float: left;
+			text-align: center;
+			width: 120px;
+			border: 1px solid gray;
+			margin: 4px;
+			padding: 6px;
+		}
+</style>
+</head>
+<body>
+	<div class="imgbox" id="imgbox1">
+	  <img src="foto.jpeg" alt="https://www.linkedin.com/in/daniel-ramirez-martinez-a5390b4b/" width="104" height="142">
+	  </div>
+	<h1>Hoja de vida</h1>
+	<h1>Daniel Camilo Ramirez Martinez</h1>
+	<p>Ingeniero de sistemas, con estudios de Maestría en resolución de conflictos y construcción de paz , MBA en Administración y Dirección de Empresas , Master en Project Management, Especialista en gestión de sistemas de información gerencial, Especialista en seguridad y gestión en bases de datos, Tecnólogo en Formulación de Proyectos.</br>
+	En el cual se destaca una gran habilidad para liderar grupos de TI, cuento con amplios conocimientos en desarrollo de y arquitectura de software.</br>
+Experto en gestión de proyectos de transformación digital para las organizaciones; con experiencia en formular proyectos innovación y de investigación en TIC, Coordinar grupos de desarrollo bajo metodologías agiles (Scrum), además me destaco por ser responsable, solidario, creativo e innovador. Capacidad de promover soluciones automatizadas.</br>
+
+	 <a href="https://www.linkedin.com/in/daniel-ramirez-martinez-a5390b4b/">Mi perfil linkedin</a>
+	 
+
+</body>
+</html>
